344643bf7c6718bb5432f3325b2d402a4f40ccca
Dif00l <55057915+Dif00l@users.noreply.github.com>
2025-10-20 18:24:34 +0200

    Update Champion.tsx

--- src/components/Champion/Champion.tsx
+++ src/components/Champion/Champion.tsx
@@ -6,11 +6,11 @@ export default function Champion(props:any){
   
 
     return(
-        <tr key="champ_"><td>{champ.Name}</td>
-            <td  style={{backgroundSize:"70%,100%,100%",backgroundPosition:"center,center",backgroundRepeat:"no-repeat",height:"87px",width:"107px",backgroundImage:'url("./images/champions/'+champ.Image+'.png"),url("./images/frames/t'+champ.Tier+'_portrait_frame.png"),url("./images/frames/class_'+champ.Klasse+'.png")'}}>
-            </td>
+        <tr key="champ_"><td>{champ.Name}</td>           
             <td>{champ.Klasse}</td>
             <td>{champ.PowerIndex}</td>
+             <td  style={{backgroundSize:"70%,100%,100%",backgroundPosition:"center,center",backgroundRepeat:"no-repeat",height:"87px",width:"107px",backgroundImage:'url("./images/champions/'+champ.Image+'.png"),url("./images/frames/t'+champ.Tier+'_portrait_frame.png"),url("./images/frames/class_'+champ.Klasse+'.png")'}}>
+            </td>
             {/*
             <td>
                 {champ.Immu.map((subItem:any, index:any) => (
